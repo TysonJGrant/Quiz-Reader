5323bbc583860ad520b4feea62a570329732d6cb
Tyson Grant <tysonjgrant@gmail.com>
2021-12-17 10:20:42 +1100

    Cleaned up some styling

--- script.js
+++ script.js
@@ -87,18 +87,28 @@ function toggleDisabled(val){
     });
 }
 
+function updateName(val){
+    let name = document.getElementById(val).innerHTML;
+    if(document.getElementById(val).className == 'collapsible active'){
+        console.log(document.getElementById(val).className + "..." + val)
+        document.getElementById(val).innerHTML = name.substring(0, name.length-1) + "▼";
+    }
+    else
+        document.getElementById(val).innerHTML = name.substring(0, name.length-1) + "▲"       
+}
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
+    console.log(i)
     coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") 
+            content.style.display = "none";
+        else 
+            content.style.display = "block";
     });
 }
         
